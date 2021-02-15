f38ae896229d2b25115aed028de212664467baa0
Ovy95 <jackoverton21@hotmail.com>
2021-02-15 12:43:46 +0000

    cleaning up scrabble of console.logs

--- src/scrabble.js
+++ src/scrabble.js
@@ -12,43 +12,29 @@ class Scrabble {
   word = word.toUpperCase()
     var i = 0;
     var len = word.length;
-    console.log(len)
+
     var score = 0
     var arr = [...word]
 
-    console.log(arr)
+
     for (; i < len;)  {
 
       console.log(arr[i]) 
       if(this.onePoint.includes(arr[i])){
         (score+=1);
-        console.log(score);
-       
       } else if(this.twoPoint.includes(arr[i])){
         (score+=2);
-        console.log(score);
-
       }else if(this.threePoint.includes(arr[i])){
         (score+=3);
-        console.log(score);
-
       }else if(this.fourPoint.includes(arr[i])){
         (score+=4);
-        console.log(score);
-
       }else if(this.fivePoint.includes(arr[i])){
         (score+=5);
-        console.log(score);
-
       }else if(this.eightPoint.includes(arr[i])){
         (score+=8);
-        console.log(score);
-
       }else if(this.tenPoint.includes(arr[i])){
         (score+=10);
-        console.log(score);
       }
-
       i++;
     }
 
